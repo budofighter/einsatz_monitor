20c1f9ed282e5a8b5b5e55319fe07fd2f79c4256
christian <chris@siebold.cloud>
2022-09-08 11:51:38 +0200

    0.9.5: Einbau Modul FWBS

---
+++ config/modul_fwbs.py
@@ -0,0 +1,8 @@
+# hier gibt es Platz für ein eigenes Modul, welches vom Alarmablauf ausgeführt wird.
+
+
+def modul_fwbs(stichwort, sachverhalt, strasse, ort, ric):
+    try:
+		print("ok")
+    except:
+        print("not ok")
