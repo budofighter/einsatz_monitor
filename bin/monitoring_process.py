cfeb9ec897f7f5663c2743421cb26b6e62d4a23e
christian <chris@siebold.cloud>
2023-08-24 16:45:54 +0200

    9.9.21: gehört zum selben Update

--- bin/monitoring_process.py
+++ bin/monitoring_process.py
@@ -1,12 +1,17 @@
 # Optimiert 30.03.23
 import time
+import sys
 import subprocess
 import logging
 import os
+import shutil
 from einsatz_monitor_modules import database_class
 from subprocess import DEVNULL
 
+
 database = database_class.Database()
+basedir = os.path.dirname(__file__)
+einsatz_process = os.path.join(basedir, "einsatz_process.py")
 
 # Logging
 logger = logging.getLogger(__name__)
@@ -35,9 +40,39 @@ def check_evaluation_server():
 def check_test_mode():
     database.update_aktiv_flag("testmode", "1" if database.select_config("testmode") == "True" else "0")
 
+def check_evaluation_status_and_reset():
+    try:
+        status = database.select_aktiv_flag("auswertung")
+        if status == 2:
+            # Lösche alle Dateien im Ordner tmp
+            tmp_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), ".." , "tmp"))
+            for filename in os.listdir(tmp_folder):
+                file_path = os.path.join(tmp_folder, filename)
+                try:
+                    if os.path.isfile(file_path) or os.path.islink(file_path):
+                        os.unlink(file_path)
+                    elif os.path.isdir(file_path):
+                        shutil.rmtree(file_path)
+                except Exception as e:
+                    logger.error(f"Failed to delete {file_path}. Reason: {e}")
+
+            # Setze den Status auf 0
+            database.update_aktiv_flag("auswertung", "0")
+
+            # Warte 10 Sekunden
+            time.sleep(10)
+
+            # Setze den Status zurück auf 1
+            database.update_aktiv_flag("auswertung", "1")
+            subprocess.Popen([sys.executable, einsatz_process])
+
+    except Exception as e:
+        logger.error(f"Error in check_evaluation_status_and_reset: {e}")
+
 
 while True:
     check_website_availability()
     check_evaluation_server()
     check_test_mode()
+    check_evaluation_status_and_reset()
     time.sleep(3)
